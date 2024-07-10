86c4509c61a57c26617e3888f1472f3ce4e2c0b7
xiedanyan <xieday@chn-das.com>
2024-07-10 11:00:03 +0800

    fix: 修复bug2

--- packages/a/index.js
+++ packages/a/index.js
@@ -1,7 +1,7 @@
 import { last } from "lodash-es";
 import isOdd from "is-odd";
 console.log("is odd", isOdd(2));
-console.log("a22");
+console.log("a223");
 export default () => {
   return last([3, 2, 1]);
 };
