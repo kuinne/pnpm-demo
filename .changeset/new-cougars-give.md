ecf34156c4e0f3f70d02cd707b421fc28ba6236b
xiedanyan <xieday@chn-das.com>
2024-07-10 11:27:20 +0800

    fix: 修复bug4

---
+++ .changeset/new-cougars-give.md
@@ -0,0 +1,5 @@
+---
+"@kuinne/a": patch
+---
+
+修复 bug4
--- packages/a/index.js
+++ packages/a/index.js
@@ -1,6 +1,6 @@
 import { last } from "lodash-es";
 import isOdd from "is-odd";
-console.log("is odd", isOdd(2));
+console.log("is odd1", isOdd(2));
 console.log("a223");
 export default () => {
   return last([3, 2, 1]);
